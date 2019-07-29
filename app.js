3f6a30d3f49cdec04a5796c7aafafadfd2714530
kpwwalsh <wnuru@hotmail.com>
2019-07-29 14:17:31 -0500

    simple server edit

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
 const http = require('http');
 const port = 3000;
+const fs = require('fs');
 
 // Handles HTTP requests.
 const requestHandler = (request, response) => {
-    fs.writeFile('hello-world.txt', 'Hello to this great world', 'utf8', (err) => {
-        console.log('success');
+    response.end('Handling a request on port ${port}') 
+    };
+fs.writeFile('hello-world.txt', 'Hello to this great world', (err) => {
+        if(err) throw err;
+        console.log('your file has been written');
     });
-    response.end('file created');
-}
 
-// Create a server and pass in the  requestHandler function
-const server = http.createServer(requestHandler);
-
-// Start the server listening on port 3000
-server.listen(port, (err) => {
-  if (err) {
-    return console.log(`You have an error:  ${err}`);
-  }
-
-  console.log(`server is listening on ${port}`);
-});
