d6e1ea6006b27339421f1c1e03b7d41ad0a538f9
kpwwalsh <wnuru@hotmail.com>
2019-07-29 22:53:00 -0500

    simple server second edit

--- app.js
+++ app.js
@@ -11,3 +11,14 @@ fs.writeFile('hello-world.txt', 'Hello to this great world', (err) => {
         console.log('your file has been written');
     });
 
+    const server = http.createServer(requestHandler);    
+
+    // Start the server listening on port 3000    
+   server.listen(port, (err) => {    
+     if (err) {    
+       return console.log(`You have an error:  ${err}`);    
+     }    
+   
+      console.log(`server is listening on ${port}`);    
+   });
+
